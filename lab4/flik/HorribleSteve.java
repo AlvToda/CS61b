b1679de92cdf8d39ba07c8fa8bfb025f02298a30
Dtt <2762530476@qq.com>
2023-10-22 17:40:37 +0800

    第一次提交lab4，将filk类中的==改成了equals

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -4,8 +4,7 @@ public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
-            //boolean flg=Flik.isSameNumber(i, j);
-            if (i!=j) {
+            if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
