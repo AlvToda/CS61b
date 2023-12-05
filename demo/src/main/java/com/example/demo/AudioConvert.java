c6900c291f5e31eb573119f25a91898b04b36308
Dtt <2762530476@qq.com>
2023-12-06 01:16:08 +0800

    新增转au文件（midi不可以作为source）和转amr,现已测试12种音频格式，还有一个bug，就是还需要注册关闭javaFX窗口的监听器

--- demo/src/main/java/com/example/demo/AudioConvert.java
+++ demo/src/main/java/com/example/demo/AudioConvert.java
@@ -160,8 +160,81 @@ public class AudioConvert {
             System.out.println("无法获取权限");
         }
     }
-    public static void main(String[] args) throws EncoderException, IOException {
+    public void ToAU(){
+        //不支持midi转au,支持flac和mp3（目前）
+        File source,target;
+        JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+        int v = chooser.showOpenDialog(null);
+        if (v == JFileChooser.APPROVE_OPTION) {
+            source = chooser.getSelectedFile();
+            target = new File(source.getAbsolutePath().substring(0, source.getAbsolutePath().indexOf(".")) + ".au");
+            if (true) {
+                System.out.println("Converting \""+source.getName()+"\" to \""+target.getName()+"\"...");
+                try {
+                    AudioAttributes audio = new AudioAttributes();
+                    audio.setCodec("pcm_alaw");
+                    audio.setBitRate(128000);
+                    audio.setChannels(2);
+                    audio.setSamplingRate(44100);
 
+                    //Encoding attributes
+                    EncodingAttributes attrs = new EncodingAttributes();
+                    attrs.setFormat("au");
+                    attrs.setAudioAttributes(audio);
+                    //encode
+                    Encoder encoder = new Encoder();
+                    encoder.encode(new MultimediaObject(source), target, attrs);}catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                System.out.println("Done");;
+            }else {
+                System.out.println("输入文件格式错误，请检查格式是否支持");
+            }
+
+        } else {
+            System.out.println("无法获取权限");
+        }
+    }
+    public static void main(String[] args) throws EncoderException, IOException {
+        //不支持midi转au
+        File source,target;
+        JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+        int v = chooser.showOpenDialog(null);
+        if (v == JFileChooser.APPROVE_OPTION) {
+            source = chooser.getSelectedFile();
+            target = new File(source.getAbsolutePath().substring(0, source.getAbsolutePath().indexOf(".")) + ".amr");
+            if (true) {
+                System.out.println("Converting \""+source.getName()+"\" to \""+target.getName()+"\"...");
+                try {
+                    //Audio Attributes
+                    AudioAttributes audio = new AudioAttributes();
+                    audio.setCodec("libvo_amrwbenc");
+                    //这里的值不可以设置过高，否则会报错，ffmpeg返回1
+                    /*audio.setBitRate(128000);
+                    audio.setChannels(1);
+                    audio.setSamplingRate(44100);*/
+                    audio.setBitRate(16000);
+                    audio.setChannels(1);
+                    audio.setSamplingRate(16000);
+
+                    //Encoding attributes
+                    EncodingAttributes attrs = new EncodingAttributes();
+                    attrs.setFormat("amr");
+                    attrs.setAudioAttributes(audio);
+                    //Encode
+                    Encoder encoder = new Encoder();
+                    encoder.encode(new MultimediaObject(source), target, attrs);
+                }catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                System.out.println("Done");;
+            }else {
+                System.out.println("输入文件格式错误，请检查格式是否支持");
+            }
+
+        } else {
+            System.out.println("无法获取权限");
+        }
 
     }
 
